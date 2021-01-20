644c91606ad9e0426dcc96139dd66afd46307328
Harry Zachariou <harry.zachariou@reachplc.com>
2021-01-20 16:37:43 +0000

    student loan and pension check

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { generateIncomeTax } = require('./incomeTax.js');
 const { nationalInsurance } = require('./nationalInsurance.js');
 const { studentLoan } = require('./studentLoan.js');
 const PENSION_PERCENTAGE = 0.06;
+const IS_STUDENT_LOAN = false;
 const SALARY = 60_000;
 const adjustedSalary = SALARY * (1 - PENSION_PERCENTAGE);
 console.log({
@@ -10,18 +11,22 @@ console.log({
     monthly: (SALARY / 12).toFixed(2),
     weekly: (SALARY / 52).toFixed(2),
   },
-  pensionPercentace: PENSION_PERCENTAGE,
-  adjustedSalary: {
-    yearly: adjustedSalary.toFixed(2),
-    monthly: (adjustedSalary / 12).toFixed(2),
-    weekly: (adjustedSalary / 52).toFixed(2),
-  },
+  ...(typeof PENSION_PERCENTAGE === 'number' && {
+    pensionPercentace: PENSION_PERCENTAGE,
+    adjustedSalary: {
+      yearly: adjustedSalary.toFixed(2),
+      monthly: (adjustedSalary / 12).toFixed(2),
+      weekly: (adjustedSalary / 52).toFixed(2),
+    },
+    pension: {
+      yearly: (SALARY * PENSION_PERCENTAGE).toFixed(2),
+      monthly: ((SALARY * PENSION_PERCENTAGE) / 12).toFixed(2),
+      weekly: ((SALARY * PENSION_PERCENTAGE) / 52).toFixed(2),
+    },
+  }),
   incomeTax: generateIncomeTax({ salary: adjustedSalary }),
   nationalInsurance: nationalInsurance({ salary: SALARY }),
-  pension: {
-    yearly: (SALARY * PENSION_PERCENTAGE).toFixed(2),
-    monthly: ((SALARY * PENSION_PERCENTAGE) / 12).toFixed(2),
-    weekly: ((SALARY * PENSION_PERCENTAGE) / 52).toFixed(2),
-  },
-  studentLoanPayments: studentLoan({ salary: SALARY }),
+  ...(IS_STUDENT_LOAN && {
+    studentLoanPayments: studentLoan({ salary: SALARY }),
+  }),
 });
