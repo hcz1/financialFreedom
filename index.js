fc205f93c80113956bb1e5dfe95d62d24ebc7811
Harry Zachariou <harry.zachariou@reachplc.com>
2021-01-20 16:32:55 +0000

    breakdown tax by time

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const PENSION_PERCENTAGE = 0.06;
 const SALARY = 60_000;
 const adjustedSalary = SALARY * (1 - PENSION_PERCENTAGE);
 console.log({
-  grossSalary: SALARY,
+  grossSalary: {
+    yearly: SALARY.toFixed(2),
+    monthly: (SALARY / 12).toFixed(2),
+    weekly: (SALARY / 52).toFixed(2),
+  },
   pensionPercentace: PENSION_PERCENTAGE,
-  adjustedSalary,
+  adjustedSalary: {
+    yearly: adjustedSalary.toFixed(2),
+    monthly: (adjustedSalary / 12).toFixed(2),
+    weekly: (adjustedSalary / 52).toFixed(2),
+  },
   incomeTax: generateIncomeTax({ salary: adjustedSalary }),
   nationalInsurance: nationalInsurance({ salary: SALARY }),
-  yearlyPension: SALARY * PENSION_PERCENTAGE,
+  pension: {
+    yearly: (SALARY * PENSION_PERCENTAGE).toFixed(2),
+    monthly: ((SALARY * PENSION_PERCENTAGE) / 12).toFixed(2),
+    weekly: ((SALARY * PENSION_PERCENTAGE) / 52).toFixed(2),
+  },
   studentLoanPayments: studentLoan({ salary: SALARY }),
 });
--- nationalInsurance.js
+++ nationalInsurance.js
@@ -10,7 +10,11 @@ const nationalInsurance = ({ salary }) => {
     niRate +=
       (4167 - 792) * NI_BAND_1 + Math.round((monthlySalary - 4167) * NI_BAND_2);
   }
-  return niRate;
+  return {
+    yearly: (niRate * 12).toFixed(2),
+    monthly: niRate.toFixed(2),
+    weekly: ((niRate * 12) / 52).toFixed(2),
+  };
 };
 // £183 to £962 a week (£792 to £4,167 a month)	12%
 // Over £962 a week (£4,167 a month)	2%
