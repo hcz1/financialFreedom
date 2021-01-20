b5b11c5803020f979e9ab814727aa5849dde6a7b
aek16 <andy.krousti@hotmail.co.uk>
2021-01-20 22:26:38 +0000

    Try again

--- incomeTaxAndy.js
+++ incomeTaxAndy.js
@@ -6,7 +6,7 @@ const generateIncomeTaxAndy = ({ salary }) => {
 
     const band1 = (salary >= 50000 ? 37500 : salary - personalAllowance) 
     
-    const band2 = (salary >= 50000 ? salary - 37500 - personalAllowance : 0)
+    const band2 = (salary >= 50000 ? (salary > 150000 ? 112500 : salary - 37500 - personalAllowance) : 0)
 
     const taxBracket1 = band1 * 0.2
     const taxBracket2 = band2 * 0.4
