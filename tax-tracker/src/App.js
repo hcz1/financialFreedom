c29bc0a8d867e9fe20f597b0b6b873396c81d4d6
Harry Zachariou <harry.zachariou@reachplc.com>
2021-01-21 21:56:26 +0000

    clearner way of rendering

--- tax-tracker/src/App.js
+++ tax-tracker/src/App.js
@@ -5,25 +5,28 @@ import s from './App.module.css';
 import Table from './containers/Table/TableContainer';
 
 const App = () => {
-  const [value, setValue] = useState(0);
-  const [hasSubmit, setHasSubmit] = useState(false);
+  const [input, setInput] = useState(0);
+  const [valueSubmit, setValueSubmit] = useState(undefined);
   const onEnter = (e) => {
     e.preventDefault();
-    setHasSubmit(true);
+    setValueSubmit(input);
   };
   const handleChange = (e) => {
-    setHasSubmit(false);
     const {
       target: { value },
     } = e;
-    setValue(value);
+    setInput(value);
   };
   return (
     <div className={s.app}>
       <h1>Fill in your yearly salary</h1>
-      <Form onEnter={onEnter} handleChange={handleChange} value={value} />
-      {hasSubmit && <h2>{`Salary submitted: £${formatNumber(value)}`}</h2>}
-      {hasSubmit && <Table className={s['table-container']} value={value} />}
+      <Form onEnter={onEnter} handleChange={handleChange} value={input} />
+      {!!valueSubmit && (
+        <>
+          <h2>{`Salary submitted: £${formatNumber(valueSubmit)}`}</h2>
+          <Table className={s['table-container']} value={valueSubmit} />
+        </>
+      )}
     </div>
   );
 };
