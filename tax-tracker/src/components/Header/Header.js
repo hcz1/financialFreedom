226ea6cd3f11ec9a46f7ec2df73f6833c1e71120
Harry Zachariou <harry.zachariou@reachplc.com>
2021-04-13 14:42:16 +0100

    implement burger menu

--- tax-tracker/src/components/Header/Header.js
+++ tax-tracker/src/components/Header/Header.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
 import s from './style.module.scss';
-const Header = ({ className }) => (
-  <div className={classnames(s.header, className)}>
-    <Link to={{ pathname: '/' }}>
-      <h1>
-        <span>
-          <b>Simple</b>Salary
-        </span>
-      </h1>
-    </Link>
-    <Link to={{ pathname: '/mortgage' }}>
-      <h1>
-        <span>Mortgage</span>
-      </h1>
-    </Link>
-  </div>
-);
+import Burger from '../Burger';
+import Menu from '../Menu';
+import { useOnClickOutside } from '../../hooks';
+const Header = ({ className }) => {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = 'main-menu';
+
+  useOnClickOutside(node, () => setOpen(false));
+  return (
+    <div className={classnames(s.header, className)}>
+      <Link to={{ pathname: '/' }}>
+        <h1>
+          <span>
+            <b>Simple</b>Salary
+          </span>
+        </h1>
+      </Link>
+      <div ref={node}>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu id={menuId} open={open} setOpen={setOpen} />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
---
+++ tax-tracker/src/components/Menu/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classnames from 'classnames';
+
+import style from './menu.module.scss';
+import { Link } from 'react-router-dom';
+
+export const Menu = ({ className, open, ...props }) => {
+  const isHidden = open ? true : false;
+  const tabIndex = isHidden ? 0 : -1;
+  return (
+    <nav
+      className={classnames(style.menu, className, open && style.open)}
+      aria-hidden={!isHidden}
+      {...props}
+    >
+      <Link to={{ pathname: '/mortgage' }} tabIndex={tabIndex}>
+        <span aria-hidden='true'>🏠</span>
+        Mortgage
+      </Link>
+    </nav>
+  );
+};
+
+export default Menu;
